b54a0d2ed10e22be0bf1f7475aca37fe13f7aa3f
Nauman Ishaq <nauman.ishaq@brainxtech.com>
2025-07-04 15:44:18 +0500

    cleaned unnecessary files

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
 app.use('/api/v1', require('./routes'))
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
